eda3ab629422bacbe2ec9d2f9e629f802c912b4b
nkknj <p8580393@gmail.com>
2025-01-02 23:01:42 +0900

    Update tq_50_gen_table_wmparc_gm.sh
    add comment

--- src/bash/tq_50_gen_table_wmparc_gm.sh
+++ src/bash/tq_50_gen_table_wmparc_gm.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
 
-# Calculating SUVR of PMPBB3 PET
-# Part 5. Calculate SUVR for cortex regions
+### THAME-Q tq_50_gen_table_wmparc_gm.sh
+### Objectives:
+# This script generates a table of SUVR values for each ROI in wmparc based on semi-quantification using the gray matter reference.
 
-# This script does
-# 1. generate cortical region of interests (ROIs) from aseg and DKT atlas
-# 2. extract mean SUVRs for each ROI
-# 3. generate a table with timestamp
+### Prerequisites:
+# - FSL: Required for image processing.
+# - FreeSurfer: Required for conversion from mgz to NIfTI format.
 
-# K. Nemoto and K. Nakayama 09 May 2023
+### Usage:
+# 1. Ensure the following files are present in the directory:
+#    - ${ID}_pmpbb3_dyn_suvr.nii
+#    - subjects/${ID}/mri/wmparc.mgz
+#    - subjects/${ID}/mri/brainstemSsLabels.v??.FSvoxelSpace.mgz
+# 2. Run the script: tq_50_gen_table_wmparc_gm.sh
 
-# For debugging
+### Main Outputs:
+# - ${ID}_pmpbb3_suvr_wmparc_mean.tsv: A table of SUVR values for each ROI in wmparc, based on the gray matter reference for ${ID}.
+# - suvr_wmparc_mean_[timestamp].tsv: A consolidated table of SUVR values for wmparc ROIs across subjects, based on the gray matter reference.
+
+### License:
+# This script is distributed under the GNU General Public License version 3.
+# See LICENSE file for details.
+
+# K. Nakayama 21 Mar 2023
+
+# For Debug
 #set -x
 
+# Load environment variable
+THAMEQDIR=$(cd $(dirname "$(realpath "$0")") ; cd ../.. ; pwd)
+source ${THAMEQDIR}/config.env
 export SUBJECTS_DIR=$PWD/subjects
 
 for f in *_pmpbb3_suvr.nii.gz
