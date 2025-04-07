e497f4515029bee48325594726fd4dede7afb92c
nkknj <p8580393@gmail.com>
2025-04-07 17:05:54 +0900

    modify brainstem-merge process

--- src/bash/tq_56_gen_table_merged_cer.sh
+++ src/bash/tq_56_gen_table_merged_cer.sh
@@ -11,7 +11,7 @@
 ### Usage:
 # 1. Ensure the following files are present in the directory:
 #    - ${ID}_pmpbb3_suvr_cer.nii.gz
-#    - ${ID}_wmparc_r.nii.gz
+#    - ${ID}_merged_r.nii.gz
 #    - subjects/${ID}/mri/brainstemSsLabels.v??.FSvoxelSpace.mgz
 # 2. Run the script: tq_56_gen_table_merged_cer.sh
 
@@ -36,21 +36,11 @@ export SUBJECTS_DIR=$PWD/subjects
 for f in *_pmpbb3_suvr_cer.nii.gz
 do
   fsid=${f%_pmpbb3_suvr_cer.nii.gz}
-  wmparc=${fsid}_wmparc_merged
+  merged=${fsid}_merged
   
-  if [[ ! -e ${wmparc}_r.nii.gz ]]; then
-    echo "Not found: ${wmparc}_r"
+  if [[ ! -e ${merged}_r.nii.gz ]]; then
+    echo "Not found: ${merged}_r"
     continue
-  fi
-  
-  # copy brainstemSsLabels*.FSvoxelSpace.mgz, add fsid, and convert to nii.gz
-  bsseg=${fsid}_bsseg
-  if [[ ! -e ${bsseg}_r.nii.gz ]]; then
-    echo "copy brainstemSsLabels.v??.FSvoxelSpace.mgz, add fsid, and convert to nii.gz"
-    find $PWD/subjects/${fsid} -name 'brainstemSsLabels.v??.FSvoxelSpace.mgz' -exec cp {} ${bsseg}.mgz \;
-    mri_label2vol --seg ${bsseg}.mgz --temp $f --o ${bsseg}_r.mgz --regheader ${bsseg}.mgz
-    mri_convert ${bsseg}_r.{mgz,nii.gz} --out_orientation $(mri_info $f | grep Orientation | awk '{ print $3 }')
-    rm *.mgz
   fi
 
   echo "extract mean SUVR within merged wmparc rois of ${fsid}"
@@ -58,22 +48,19 @@ do
   
   # divide wmparc_merged.nii.gz into five subregions and calc mean for each region
   # aseg and brainstem
-  fslmaths ${wmparc}_r.nii.gz -uthr 999.5 ${wmparc}_r_0000.nii.gz
-  fslstats -K ${wmparc}_r_0000.nii.gz $f -M > tmp_${fsid}_suvr_cer_wmparc_merged_mean_0000
-  cat tmp_${fsid}_suvr_cer_wmparc_merged_mean_0000 | sed -n '7,8p;10,13p;17,18p;26p;28p;46,47p;49,54p;58p;60p;251,255p' >> ${fsid}_pmpbb3_suvr_cer_wmparc_merged_mean.tsv
-  
-  fslstats -K ${bsseg}_r.nii.gz $f -M > tmp_${fsid}_suvr_cer_bsseg_mean
-  cat tmp_${fsid}_suvr_cer_bsseg_mean | sed -n '173,175p;178p' >> ${fsid}_pmpbb3_suvr_cer_wmparc_merged_mean.tsv
-  
+  fslmaths ${merged}_r.nii.gz -uthr 999.5 ${merged}_r_0000.nii.gz
+  fslstats -K ${merged}_r_0000.nii.gz $f -M > tmp_${fsid}_suvr_cer_wmparc_merged_mean_0000
+  cat tmp_${fsid}_suvr_cer_wmparc_merged_mean_0000 | sed -n '7,8p;10,13p;17,18p;26p;28p;46,47p;49,54p;58p;60p;173,175p;251,255p' >> ${fsid}_pmpbb3_suvr_cer_wmparc_merged_mean.tsv
+
   # 1000: lt cortex; 2000: rt cotex 
   # 3000: lt subcortical wm; 4000: rt subcortical wm
   for num in 1000 2000 3000 4000
   do
     lthr=$((num - 1))
     uthr=$((num + 999))
-    fslmaths ${wmparc}_r.nii.gz -thr ${lthr}.5 -uthr ${uthr}.5 -sub ${num} \
-      ${wmparc}_r_${num}.nii.gz
-    fslstats -K ${wmparc}_r_${num}.nii.gz $f -M >\
+    fslmaths ${merged}_r.nii.gz -thr ${lthr}.5 -uthr ${uthr}.5 -sub ${num} \
+      ${merged}_r_${num}.nii.gz
+    fslstats -K ${merged}_r_${num}.nii.gz $f -M >\
         tmp_${fsid}_suvr_cer_wmparc_merged_mean_${num}
     cat tmp_${fsid}_suvr_cer_wmparc_merged_mean_${num} |\
         sed -n '1,3p;5,9p;11,13p;15,18p;21,22p;24,25p;28,31p;33,35p' >> ${fsid}_pmpbb3_suvr_cer_wmparc_merged_mean.tsv
@@ -104,15 +91,14 @@ Rt-Hippocampus
 Rt-Amygdala
 Rt-Accumbens-area
 Rt-VentralDC
+Midbrain
+Pons
+Medulla
 CC_Posterior
 CC_Mid_Posterior
 CC_Central
 CC_Mid_Anterior
 CC_Anterior
-Midbrain
-Pons
-Medulla
-SCP
 Lt-bankssts
 Lt-cingulate
 Lt-middlefrontal
@@ -223,6 +209,6 @@ echo "generate table"
 timestamp=$(date +%Y%m%d_%H%M)
 paste colheader_merged.txt *_suvr_cer_wmparc_merged_mean.tsv > suvr_cer_wmparc_merged_mean_${timestamp}.tsv
 
-rm colheader_merged.txt tmp_*_suvr_cer_wmparc_merged_mean_?000 tmp_*_suvr_cer_bsseg_mean *_r_?000.nii.gz
+rm colheader_merged.txt tmp_*_suvr_cer_wmparc_merged_mean_?000 *_r_?000.nii.gz
 
 echo "Done. Please check suvr_cer_wmparc_merged_mean_${timestamp}.tsv"
