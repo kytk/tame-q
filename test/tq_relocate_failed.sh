d49a2485bd2db9db62c3950898fcda9294425d84
nkknj <p8580393@gmail.com>
2025-04-23 14:51:49 +0900

    bugfix

--- test/tq_relocate_failed.sh
+++ test/tq_relocate_failed.sh
@@ -18,7 +18,7 @@ for d in $(find ./failed -maxdepth 2 -name "$ID" -type d); do
   mkdir -p histogram_GMref histogram_WMref subjects
   [[ -e ${d}/histogram_GMref ]] && find ${d}/histogram_GMref -type f -exec mv {} ./histogram_GMref \;
   [[ -e ${d}/histogram_WMref ]] && find ${d}/histogram_WMref -type f -exec mv {} ./histogram_WMref \;
-  [[ -e ${d}/subjects ]] && find ${d}/subjects -type d -name ${ID} -exec mv {} ./subjects \;
+  [[ -e ${d}/subjects/${ID} ]] && mv ${d}/subjects/${ID} ./subjects;
   find ${d} -maxdepth 1 -type f -exec mv {} . \;
   
   if ! find $d -type f | grep -q .; then
