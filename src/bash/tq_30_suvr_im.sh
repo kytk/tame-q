036546f29fbb3fcc1f7a8eaf3f3cd9f8e67f144f
nkknj <p8580393@gmail.com>
2026-03-30 10:31:24 +0900

    Correct weighted mean calculation

--- src/bash/tq_30_suvr_im.sh
+++ src/bash/tq_30_suvr_im.sh
@@ -20,8 +20,8 @@ cleanup() {
     status=$?
     if [[ "${status}" -eq 0 ]] && [[ "${cache}" = false ]]; then
         rm -f ${pet_tuned_gm} ${pet_tuned_wm}
-        rm -f ${subjectdir}/${gmtmpimg}*
-        rm -f ${subjectdir}/${wmtmpimg}*
+        #rm -f ${subjectdir}/${gmtmpimg}*
+        #rm -f ${subjectdir}/${wmtmpimg}*
     fi
     jobs -pr | xargs -r kill 2>/dev/null || true
 }
@@ -79,14 +79,20 @@ pet=${subjectdir}/pet_mean.nii.gz
 pet_tuned_gm=${pet/mean/tuned_by_gm}
 pet_tuned_wm=${pet/mean/tuned_by_wm}
 c1mask=${subjectdir}/c1mri_mni.nii.gz
+c1mask_thr=${subjectdir}/c1mri_mni_thr.nii.gz
 c2mask=${subjectdir}/c2mri_mni.nii.gz
+c2mask_thr=${subjectdir}/c2mri_mni_thr.nii.gz
 refpolicy=${subjectdir}/reference_policy.py
 check_existence ${pet} ${c1mask} ${c2mask}
 
 ### Gray matter
+# Threshold probability mask
+fslmaths ${c1mask} -thr 0.9 ${c1mask_thr}
+
 # Erosion for c1 mask image
 echo "c1mask erosion (option: ${C1_EROSION_OPTION})"
-${TAMEQDIR}/src/bash/tq-erode.sh ${c1mask} ${subjectdir}/mask_gm_for_hist.nii.gz ${C1_EROSION_OPTION} ${debug_option}
+${TAMEQDIR}/src/bash/tq-erode.sh ${c1mask_thr} ${subjectdir}/mask_gm_for_hist.nii.gz ${C1_EROSION_OPTION} ${debug_option}
+fslmaths ${subjectdir}/mask_gm_for_hist.nii.gz -bin ${subjectdir}/mask_gm_for_hist.nii.gz
 
 # Tune signal intensity for curve fit
 echo "Tuning signals for curve fitting"
@@ -135,9 +141,10 @@ ${TAMEQDIR}/src/bash/tq-refgen.sh \
 
 # Calculate reference value
 echo -e "\nReference calculation (GM) ..."
-gmtmpimg=tq-all_tmp$(randomphrase 8)
-fslmaths ${pet_tuned_gm} -mul ${subjectdir}/reference_gm.nii.gz ${subjectdir}/${gmtmpimg}
-reference_value_gm=$(fslstats ${subjectdir}/${gmtmpimg} -M)
+#gmtmpimg=tq-all_tmp$(randomphrase 8)
+#fslmaths ${pet_tuned_gm} -mul ${subjectdir}/reference_gm.nii.gz ${subjectdir}/${gmtmpimg}
+#reference_value_gm=$(fslstats ${subjectdir}/${gmtmpimg} -M)
+reference_value_gm=$(${TAMEQDIR}/src/python/nifti_wmean.py ${pet_tuned_gm} ${subjectdir}/reference_gm.nii.gz)
 echo "Reference value = ${reference_value_gm}"
 
 # Devide PET image by reference value for semi-quantification
@@ -145,9 +152,13 @@ fslmaths ${pet_tuned_gm} -div ${reference_value_gm} ${pet/mean/suvr_gm}
 echo "  --> Create SUVR image (${pet/mean/suvr_gm})"
 
 ### White Matter
+# Threshold probability mask
+fslmaths ${c2mask} -thr 0.9 ${c2mask_thr}
+
 # Erosion for c2 mask image
 echo "c2mask erosion (option: ${C2_EROSION_OPTION})"
-${TAMEQDIR}/src/bash/tq-erode.sh ${c2mask} ${subjectdir}/mask_wm_for_hist.nii.gz ${C2_EROSION_OPTION} ${debug_option}
+${TAMEQDIR}/src/bash/tq-erode.sh ${c2mask_thr} ${subjectdir}/mask_wm_for_hist.nii.gz ${C2_EROSION_OPTION} ${debug_option}
+fslmaths ${subjectdir}/mask_gm_for_hist.nii.gz -bin ${subjectdir}/mask_gm_for_hist.nii.gz
 
 # Tune signal intensity for curve fit
 echo "Tuning signals for curve fitting"
@@ -195,9 +206,10 @@ ${TAMEQDIR}/src/bash/tq-refgen.sh \
 
 # Calculate reference value
 echo -e "\nReference calculation (WM) ..."
-wmtmpimg=tq-all_tmp$(randomphrase 8)
-fslmaths ${pet_tuned_wm} -mul ${subjectdir}/reference_wm.nii.gz ${subjectdir}/${wmtmpimg}
-reference_value_wm=$(fslstats ${subjectdir}/${wmtmpimg} -M)
+#wmtmpimg=tq-all_tmp$(randomphrase 8)
+#fslmaths ${pet_tuned_wm} -mul ${subjectdir}/reference_wm.nii.gz ${subjectdir}/${wmtmpimg}
+#reference_value_wm=$(fslstats ${subjectdir}/${wmtmpimg} -M)
+reference_value_wm=$(${TAMEQDIR}/src/python/nifti_wmean.py ${pet_tuned_wm} ${subjectdir}/reference_wm.nii.gz)
 echo "Reference value = ${reference_value_wm}"
 
 # Devide PET image by reference value for semi-quantification
