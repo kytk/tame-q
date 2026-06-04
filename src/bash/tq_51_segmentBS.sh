67ea490df07747d8dc792bd784e807d9fd378de8
nkknj <p8580393@gmail.com>
2026-06-04 11:09:43 +0900

    Erase IsRunningBSsubst before segmentBS

--- src/bash/tq_51_segmentBS.sh
+++ src/bash/tq_51_segmentBS.sh
@@ -87,6 +87,7 @@ fi
 
 ### Process
 if [[ $(find ${subjectdir}/freesurfer/${ID}/mri -name "brainstemSsLabels.v??.FSvoxelSpace.mgz" | wc -l) -lt 1 ]]; then
+    rm -f ${subjectdir}/freesurfer/${ID}/scripts/IsRunningBSsubst
     while [[ "$(pgrep -x segmentBS.sh -c)" -ge ${MAX_SEGMENTBS} ]]; do sleep 10s ; done
     echo "Run > segmentBS.sh ${ID} ${SUBJECTS_DIR}"
     segmentBS.sh ${ID} ${SUBJECTS_DIR} > /dev/null
